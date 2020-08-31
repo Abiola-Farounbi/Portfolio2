64b73448001857c4e6ac1ed0904e217b3e43f2fc
abiola farounbi <farounbiabiola@gmail.com>
2020-09-01 00:44:18 +0100

    made new changes

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+
+
+
+
+
+hamburger.addEventListener('click', ()=>{
+    //Animate Link
+navLinks.classList.toggle("open");
+navLinks.style.display='flex'
+links.forEach( link => {
+link.classList.toggle("fade");
+link.addEventListener('click' , () => {
+    navLinks.style.display='none';
+});
+});
+// Hamburger Animation
+hamburger.classList.toggle("toggle");
+});
+
+
+
+var options = {
+    strings: ["", "Farounbi~ ", "Abiola~", "Esther~", "Farounbi Abiola Esther" ],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 30,
+    // time before typing starts
+    startDelay: 1200,
+    // backspacing speed
+    backSpeed: 20,
+    // time before backspacing
+    backDelay: 500,
+    showCursor: false,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html',
+    // call when done callback function
+};
+
+var typed = new Typed('.Text', options);
+
+
+  
