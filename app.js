35b4506678e30fa18363d1c053e0766a63f8c779
Abiola-Farounbi <farounbiabiola@gmail.com>
2021-11-19 21:10:37 +0100

    Added beautiful features to my blog

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// / Filter Tags
+const blogs =  document.querySelectorAll(".blog");
+const  vueTag = document.querySelectorAll(".vue");
+const  cssTag = document.querySelectorAll(".css");
+const  jsTag = document.querySelectorAll(".js");
+const  pythonTag = document.querySelectorAll(".python");
+const  devTag = document.querySelectorAll(".dev");
+
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
@@ -42,4 +50,52 @@ var options = {
 var typed = new Typed('.Text', options);
 
 
-  
+
+
+function hide_init(){
+    // var blogs =  document.querySelectorAll('.blog');
+    for (var i = 0; i < blogs.length; ++i) { 
+            blogs[i].style.display = 'none'; }
+ }
+
+ function showAll(){  
+    //loop through all items and show them
+     for (var i = 0; i < blogs.length; ++i) { 
+        blogs[i].style.display = 'block'; } 
+  }
+
+  function showVue(){  
+    //call the inital hide function
+    hide_init();
+    //loop through red-tagged items and show them  
+       for (var i = 0; i < vueTag.length; ++i) { 
+            vueTag[i].style.display = 'block';  } 
+    }
+
+    function showCss(){ 
+        hide_init();
+           for (var i = 0; i < cssTag.length; ++i) { 
+                cssTag[i].style.display = 'block';  } 
+        }
+
+    function showJs(){  
+        hide_init();
+           for (var i = 0; i < jsTag.length; ++i) { 
+                jsTag[i].style.display = 'block';  } 
+        }
+
+    function showPython(){  
+        hide_init();
+           for (var i = 0; i < pythonTag.length; ++i) { 
+                pythonTag[i].style.display = 'block';  } 
+        }
+        
+    function showDev(){  
+        hide_init();
+           for (var i = 0; i < devTag.length; ++i) { 
+                devTag[i].style.display = 'block';  } 
+        }
+    
+
+
+
