17d858fc85551be37019ea94552d2c387dbbe5cb
Abiola-Farounbi <farounbiabiola@gmail.com>
2023-01-19 15:42:03 +0100

    Fixed typing animation bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-
+// Navbar
 hamburger.addEventListener('click', ()=>{    
 navLinks.classList.toggle("open");
 navLinks.style.display='flex'
@@ -17,30 +17,6 @@ hamburger.classList.toggle("close");
 });
 
 
-
-var options = {
-    strings: ["", "Farounbi~ ", "Abiola~",  "Farounbi Abiola" ],
-    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-    stringsElement: null,
-    // typing speed
-    typeSpeed: 30,
-    // time before typing starts
-    startDelay: 1200,
-    // backspacing speed
-    backSpeed: 20,
-    // time before backspacing
-    backDelay: 500,
-    showCursor: false,
-    // character for cursor
-    cursorChar: "|",
-    // attribute to type (null == text)
-    attr: null,
-    // either html or text
-    contentType: 'html',
-    // call when done callback function
-};
-
-var typed = new Typed('.Text', options);
 
 
 // / Filter Tags
